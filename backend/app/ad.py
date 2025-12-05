6a2927850e51c01e15423abb405f577ac5a3896d
samat006 <asamathseck@gmail.com>
2025-12-05 15:03:24 +0100

    ajout de AD .env

--- backend/app/ad.py
+++ backend/app/ad.py
@@ -1,23 +1,36 @@
-from ldap3 import Server, Connection, ALL, NTLM
+from ldap3 import Server, Connection, ALL
+from dotenv import load_dotenv
+import os
 
-# -------------------- FONCTIONS UTILISATEUR AD --------------------
-
+# Charger le fichier .env
+load_dotenv()
 
 # -------------------- CONFIGURATION AD --------------------
-AD_SERVER = "34.201.92.124"        # IP du serveur AD
-AD_USER = "a2s@m2data.local"  # compte admin pour lecture LDAP
-AD_PASSWORD = "12345678aS"
-AD_BASE_DN = "DC=M2DATA,DC=LOCAL"
+
+
+AD_SERVER = os.getenv("AD_SERVER")
+AD_USER = os.getenv("AD_USER")
+AD_PASSWORD = os.getenv("AD_PASSWORD")
+AD_BASE_DN = os.getenv("AD_BASE_DN")
+
 
 server = Server(AD_SERVER, get_info=ALL)
 conn = Connection(server, AD_USER, AD_PASSWORD, auto_bind=True)
 
+# -------------------- FONCTIONS UTILISATEUR AD --------------------
 
 def get_user_info(login):
-    # Récupère le nom et email depuis AD pour un login donné
+
+    """Récupère le nom et email depuis AD pour un login donné."""
+
     search_filter = f"(sAMAccountName={login})"
     conn.search(AD_BASE_DN, search_filter, attributes=["displayName", "mail"])
+    
     if conn.entries:
         entry = conn.entries[0]
-        return {"displayName": str(entry.displayName), "mail": str(entry.mail)}
+        return {
+            "displayName": str(entry.displayName),
+            "mail": str(entry.mail)
+        }
+    
     return {"displayName": login, "mail": None}
